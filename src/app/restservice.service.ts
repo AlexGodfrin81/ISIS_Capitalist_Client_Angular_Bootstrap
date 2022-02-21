dd9aa3a56c5a309d5ab0158c9cf0ef3b779204a9
agodfrin <alex.godfrin@etud.univ-jfc.fr>
2022-02-21 10:54:37 +0100

    modif resetservice.service.ts

--- src/app/restservice.service.ts
+++ src/app/restservice.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+import { World, Pallier, Product } from './world';
 
 @Injectable({
   providedIn: 'root'
@@ -8,5 +10,4 @@ export class RestserviceService {
   constructor() { }
 }
 
-import { HttpClient } from '@angular/common/http'
-import { World, Pallier, Product } from './world';
+
